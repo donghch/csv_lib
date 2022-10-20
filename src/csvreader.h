56f9f92ecbf91e45b82579b2c155e951b1ab166b
donghch <2296981873@qq.com>
2022-10-20 02:40:31 -0700

    Updated files

---
+++ src/csvreader.h
@@ -0,0 +1,29 @@
+/**
+ * csvreader
+ * 
+ * Purpose:     This file is designed for read and parsing csv data
+ * Author:      Houchuan(Henry) Dong
+ * Repo link:   https://github.com/donghch/csv_lib
+ * Date: Oct 20 2022
+*/
+#define CSVREADER_LOADED
+
+#include <stdio.h>
+
+/**
+ * getwidth
+ * 
+ * Purpose:     Get csv file's data width.
+ * Requires:    A valid csv file defined by RFC4180 standard.
+ * Returns:     The data width of the file.
+*/
+int getwidth(FILE fileHandle);
+
+/**
+ * getlength
+ * 
+ * Purpose:     Get csv file's length
+ * Requires:    A valid csv file defined by RFC4180 standard.
+ * Returns:     The length of the file
+*/
+int getlength(FILE fileHandle);
